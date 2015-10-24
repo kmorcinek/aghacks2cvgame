46574a7b06989d4bae928a381b990fe3069068a2
hawkerpl <m.m.hawker@gmail.com>
2015-10-24 23:42:02 +0200

    restruct of cherrypy

--- python_server/python_server.py
+++ python_server/python_server.py
@@ -14,44 +14,32 @@ with open('python_server/vision/majortestsmaller2.jpg') as f:
 
 
 class StringGeneratorWebService(object):
-    exposed = True
     data = {"testowa_gra":testowa_gra}
-    @cherrypy.tools.accept(media='application/json')
-    @cherrypy.tools.json_out()
-    @cherrypy.tools.json_in()
-    def POST(self):
+
+    @cherrypy.expose
+    def send_android(self):
         input_json = cherrypy.request.json
         value = input_json["picture"]
         game_name = input_json["game_name"]
         decoded = base64.b64decode(value[0])
         self.data[game_name] = decoded
-        #print decoded
-        #db = sqlite3.connect(DATABASE_PATH)
-        #db.execute('CREATE TABLE t (thebin BLOB)')
-        #db.execute('INSERT INTO pictures VALUES(?)', [buffer(decoded),game_name])
-        #db.commit()  
-        #db.close()
-        #with open('/home/lukasz/Programowanie/hackaton/img.jpg', 'wb')as file:
-        #    file.write(decoded)
         result = {"operation": "request", "result": "success"}
         return result
 
-
-    def GET(self,game_name,pic=""):
-        if pic == "":
-            detector = vision.dummy_wrapper.Detect(self.data[game_name])
-            return detector.detect()
-        else:
-            cherrypy.response.headers['Content-Type'] = "image/jpg"
-            return self.data[game_name]
+    @cherrypy.expose
+    def detector(self,game_name):
+        detector = vision.dummy_wrapper.Detect(self.data[game_name])
+        return detector.detect()
+        
+    @cherrypy.expose
+    def zdjecie(self,game_name):
+        cherrypy.response.headers['Content-Type'] = "image/jpg"
+        return self.data[game_name]
 
 if __name__ == '__main__':
     conf = {
         '/': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.sessions.on': True,
             'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'application/json')],
         }
     }
     cherrypy.config.update({'server.socket_host': '0.0.0.0',
