6524d762aea09cf4e7e33d6e58938aa4d09af66d
Łukasz Mitka <luk.mitka@uj.edu.pl>
2015-10-24 21:33:23 +0200

    wersja dzialajaca na tablecie

---
+++ CvGame2/app/src/main/java/aghacks/cvgame/CameraPreview.java
@@ -0,0 +1,8 @@
+package aghacks.cvgame;
+
+/**
+ * Created by lukasz on 24.10.15. This file is element of RESTful Jive application project. You are free to use, copy and
+ * edit whole application or any of its components. Application comes with no warranty. Altough author is trying to make it
+ * best, it may work or it may not work.
+ */
+public class CameraPreview {}
